dee9d6d657230d44b3ad03b646edb6daae4e214a
Vijayalakshmi G (she/her) <104077094+VijayalakshmiGanesh@users.noreply.github.com>
2023-06-23 12:11:46 +0530

    Added bookmarks list to context

--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -1,12 +1,17 @@
 import { createContext, useContext, useReducer } from "react";
 import { dataReducer, dataInitialValue } from "../redux/data";
-
 export const DataContext = createContext();
 export const DataProvider = ({ children }) => {
   const [state, dispatch] = useReducer(dataReducer, dataInitialValue);
+
   return (
     <DataContext.Provider
-      value={{ state, datadispatch: dispatch, posts: state?.posts }}
+      value={{
+        state,
+        datadispatch: dispatch,
+        posts: state?.posts,
+        bookmarks: state?.bookmarkPosts,
+      }}
     >
       {children}
     </DataContext.Provider>
