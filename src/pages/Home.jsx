278624f17e433f5f60bad86c104944deb58f0a07
Vijayalakshmi G (she/her) <104077094+VijayalakshmiGanesh@users.noreply.github.com>
2023-06-23 12:10:36 +0530

    feed filters posts based on following list

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,8 @@ import { AiFillCaretDown, AiFillCaretUp } from "react-icons/ai";
 import { BiTrendingUp } from "react-icons/bi";
 import { v4 as uuid } from "uuid";
 import Avatar from "react-avatar";
+import { AdvancedImage } from "@cloudinary/react";
+import { Cloudinary } from "@cloudinary/url-gen";
 
 import { getAllPosts, newPost } from "../services/Posts";
 import { useData } from "../context/DataContext";
@@ -22,10 +24,25 @@ function Home() {
   const [displaySortOption, setDisplaySortOption] = useState(false);
   const [filteredPosts, setFilteredPosts] = useState(posts);
   const [newPostContent, setNewPostContent] = useState("");
-  // const [isEditingPost, setIsEditingPost] = useState({})
+
   const filterPosts = () => {
+    const currentFollowing = currentUserDetails?.following?.map(
+      ({ username }) => username
+    );
+    if (currentFollowing.length === 0) return [];
+
+    console.log("Cuureent followings", currentFollowing);
+    console.log(
+      "filtering",
+      posts.filter(({ username }) => currentFollowing.includes(username))
+    );
+    const filteredPostsBasedOnFollowing =
+      currentFollowing?.length >= 1
+        ? posts?.filter(({ username }) => username?.includes(currentFollowing))
+        : [];
+    console.log("filteredPostsBasedOnFollowing", filteredPostsBasedOnFollowing);
     return [
-      ...posts?.sort((a, b) => {
+      ...filteredPostsBasedOnFollowing?.sort((a, b) => {
         if (sortPostsBy === "Oldest") {
           return new Date(a.createdAt) - new Date(b.createdAt);
         } else if (sortPostsBy === "Latest") {
@@ -58,15 +75,16 @@ function Home() {
     setNewPostContent("");
   };
 
-  console.log("posts", filteredPosts);
+  console.log("posts", posts);
+
+  useEffect(() => {
+    setFilteredPosts([...filterPosts()]);
+  }, [sortPostsBy, posts, currentUserDetails?.following]);
+
   useEffect(() => {
     getAllPosts(datadispatch);
     getAllUsers(userDispatch);
   }, []);
-
-  useEffect(() => {
-    setFilteredPosts([...filterPosts()]);
-  }, [sortPostsBy, posts]);
   return (
     <div className="flex justify-center">
       <NavBar />
@@ -128,115 +146,14 @@ function Home() {
           )}
         </div>
         <div className="flex flex-col items-center m-2 flex-wrap">
-          {posts?.length === 0 ? (
+          {filteredPosts?.length === 0 ? (
             <p>No Posts</p>
           ) : (
             filteredPosts?.map((post) => {
               return <PostCard post={post} />;
-              // const { _id, username, content, likes, fullName } = post;
-
-              // return (
-              //   <div
-              //     className="m-3 border border-2 px-3 py-5 w-[35rem] bg-[#2d2f3e] flex"
-              //     key={_id}
-              //   >
-              //     <Avatar
-              //       color={Avatar.getRandomColor("sitebase", [
-              //         // "rgb(251 146 60)",
-              //         "black",
-
-              //         "blue",
-              //         "pink",
-              //       ])}
-              //       name={fullName}
-              //       size="50"
-              //       round={true}
-              //     />
-              //     <div className="px-2 mx-2 grow">
-              //       <p className="flex justify-between">
-              //         <span>
-              //           {fullName} &nbsp;
-              //           <span className="text-pink-400">@{username}</span>
-              //         </span>
-              //         <div
-              //           className="relative"
-              //           onMouseOver={() =>
-              //             setDisplayPostOptions((prev) => !prev)
-              //           }
-              //           onMouseOut={() => setDisplayPostOptions(false)}
-              //         >
-              //           <button
-              //           // onMouseOver={() =>
-              //           //   setDisplayPostOptions((prev) => !prev)
-              //           // }
-              //           // onMouseOut={() => setDisplayPostOptions(false)}
-              //           >
-              //             <BsThreeDots />
-              //           </button>
-              //           {username === currentUserDetails.username &&
-              //             displayPostOptions && (
-              //               <div className="absolute top-5 right-0 w-[7rem] h-fit flex flex-col border border-2 z-30 bg-primary rounded">
-              //                 <button
-              //                   className="p-2  border-b-2 "
-              //                   onClick={
-              //                     () => setIsEditingPost(_id)
-              //                     // editPost(_id, post, datadispatch)
-              //                   }
-              //                 >
-              //                   Edit
-              //                 </button>
-              //                 <button
-              //                   className="p-2"
-              //                   onClick={() => deletePost(_id, datadispatch)}
-              //                 >
-              //                   Delete
-              //                 </button>
-              //               </div>
-              //             )}
-              //         </div>
-              //       </p>
-              //       {isEditingPost === _id ? (
-              //         <textarea value={content}>{content}</textarea>
-              //       ) : (
-              //         <p className="text-left">{content}</p>
-              //       )}
-              //       <p className="my-2 flex justify-between">
-              //         <button
-              //           className="rounded-full  hover:bg-red-300/[0.1] p-2 hover:text-red-700 flex items-center "
-              //           onClick={() =>
-              //             checkLikedList(likes.likedBy)
-              //               ? likePost(_id, posts, datadispatch)
-              //               : unlikePost(_id, posts, datadispatch)
-              //           }
-              //         >
-              //           {checkLikedList(likes.likedBy) ? (
-              //             <AiOutlineHeart className="text-xl" />
-              //           ) : (
-              //             <AiFillHeart className="text-red-700 text-xl" />
-              //           )}
-              //           <span className="text-lg pl-2 ">{likes.likeCount}</span>
-              //         </button>
-              //         {isEditingPost === _id && (
-              //           <div>
-              //             <button
-              //               onClick={() => {
-              //                 editPost(_id, post, datadispatch);
-              //                 setIsEditingPost("");
-              //               }}
-              //             >
-              //               Save
-              //             </button>
-              //             <button onClick={() => setIsEditingPost("")}>
-              //               Cancel
-              //             </button>
-              //           </div>
-              //         )}
-              //       </p>
-              //     </div>
-              //   </div>
-              // );
             })
           )}
+          <AdvancedImage cldImg={myImage} />
         </div>
       </div>
       <Aside />
