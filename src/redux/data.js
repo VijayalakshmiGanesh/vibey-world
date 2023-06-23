7085d7eea728899258de777e1674d5187b55b636
Vijayalakshmi G (she/her) <104077094+VijayalakshmiGanesh@users.noreply.github.com>
2023-06-23 12:07:19 +0530

    Added bookmarks to reducer

--- src/redux/data.js
+++ src/redux/data.js
@@ -1,11 +1,15 @@
 export const dataInitialValue = {
   posts: [],
+  bookmarkPosts: [],
 };
 
 export const dataReducer = (result, action) => {
   switch (action.type) {
     case "GET_LISTS_OF_POSTS":
       return { ...result, posts: action.payload };
+
+    case "GET_ALL_BOOKMARKED_POSTS":
+      return { ...result, bookmarkPosts: action.payload };
 
     default:
       return result;
