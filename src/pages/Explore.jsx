bc8dab5bbc1728a854bb48ea1b0d1aefc39032c9
Vijayalakshmi G (she/her) <104077094+VijayalakshmiGanesh@users.noreply.github.com>
2023-07-03 12:35:26 +0530

    Added css and no post message

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,19 +1,37 @@
+import { AiOutlineArrowLeft } from "react-icons/ai";
+
 import Aside from "../component/Aside";
 import NavBar from "../component/NavBar";
 import { useData } from "../context/DataContext";
 import PostCard from "../component/PostCard";
+import { useNavigate } from "react-router";
 
 function Explore() {
   const { posts } = useData();
+  const navigate = useNavigate();
   const filteredPosts = [...posts.sort((a, b) => b.createdAt - a.createdAt)];
+
   return (
-    <div className="flex justify-center">
-      <NavBar />
-      <div>
-        <p>EXPLORE</p>
+    // <div className="flex justify-start">
+    //   <div className=" px-5 pr-[13rem]">
+    //     <NavBar />
+    //   </div>
+    <div>
+      <div className="w-[40rem]">
+        <div className="flex justify-start p-3 my-1">
+          <button
+            onClick={() => navigate(-1)}
+            className="p-1 text-2xl hover:border hover:border-2 hover:border-gray-700 hover:bg-[#2d2f3ea8] rounded-lg"
+          >
+            <AiOutlineArrowLeft />
+          </button>
+          <p className="flex flex-col items-start px-5 text-xl">Explore</p>
+        </div>
         <div className="flex flex-col items-center m-2 flex-wrap">
-          {posts?.length === 0 ? (
-            <p>No Posts</p>
+          {filteredPosts?.length === 0 ? (
+            <p className="bg-red-500/[0.3] w-full text-xl p-3 my-3 rounded">
+              No Posts
+            </p>
           ) : (
             filteredPosts?.map((post) => {
               return <PostCard post={post} />;
@@ -21,7 +39,7 @@ function Explore() {
           )}
         </div>
       </div>
-      <Aside />
+      {/* <Aside /> */}
     </div>
   );
 }
