ccd6129eff34bcc746323d041cf854b6a28aabfd
Vijayalakshmi G (she/her) <104077094+VijayalakshmiGanesh@users.noreply.github.com>
2023-07-03 12:36:04 +0530

    Added css and no post message

--- src/pages/Bookmarks.jsx
+++ src/pages/Bookmarks.jsx
@@ -1,3 +1,6 @@
+import { useNavigate } from "react-router";
+import { AiOutlineArrowLeft } from "react-icons/ai";
+
 import Aside from "../component/Aside";
 import NavBar from "../component/NavBar";
 import PostCard from "../component/PostCard";
@@ -5,23 +8,39 @@ import { useData } from "../context/DataContext";
 
 function Bookmarks() {
   const { posts, bookmarks } = useData();
+  const navigate = useNavigate();
 
   const getBookmarkedPostsDetails = posts.filter(({ _id }) => {
     return bookmarks.includes(_id);
   });
 
   return (
-    <div className="flex justify-center">
-      <NavBar />
-      <div>
-        <p>Bookmarks</p>
-        {bookmarks.length === 0 ? (
-          <p>No bookmarks</p>
-        ) : (
-          getBookmarkedPostsDetails.map((post) => <PostCard post={post} />)
-        )}
+    // <div className="flex justify-start">
+    //   <div className=" px-5 pr-[13rem]">
+    //     <NavBar />
+    //   </div>
+    <div>
+      <div className="w-[40rem]">
+        <div className="flex justify-start p-3 my-1">
+          <button
+            onClick={() => navigate(-1)}
+            className="p-1 text-2xl hover:border hover:border-2 hover:border-gray-700 hover:bg-[#2d2f3ea8] rounded-lg"
+          >
+            <AiOutlineArrowLeft />
+          </button>
+          <p className="flex flex-col items-start px-5 text-xl">Bookmarks</p>
+        </div>
+        <div className="flex flex-col items-center m-2 flex-wrap">
+          {bookmarks.length === 0 ? (
+            <p className="bg-red-500/[0.3] w-full text-xl p-3 my-3 rounded">
+              No bookmarked posts
+            </p>
+          ) : (
+            getBookmarkedPostsDetails.map((post) => <PostCard post={post} />)
+          )}
+        </div>
       </div>
-      <Aside />
+      {/* <Aside /> */}
     </div>
   );
 }
