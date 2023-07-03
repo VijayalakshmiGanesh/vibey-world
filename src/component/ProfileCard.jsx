cdf8aacc41b5ed49f535ac24cc63d600081fc2d8
Vijayalakshmi G (she/her) <104077094+VijayalakshmiGanesh@users.noreply.github.com>
2023-07-03 12:36:33 +0530

    Removed border from profile card

--- src/component/ProfileCard.jsx
+++ src/component/ProfileCard.jsx
@@ -2,7 +2,7 @@ import Avatar from "react-avatar";
 
 function ProfileCard({ name, username, imageURL }) {
   return (
-    <div className="flex border border-gray-700 p-2 m-1 rounded ">
+    <div className="flex p-2 m-1 rounded-lg  hover:surface-clr">
       {/* <div className="bg-gradient-to-r from-[#054cff] from-10%  to-90% via-85% to-[#ff059b] via-[#ffa111] rounded-full py-1"> */}
       <span className="px-1 rounded-full">
         {!imageURL || imageURL?.length === 0 ? (
@@ -27,7 +27,7 @@ function ProfileCard({ name, username, imageURL }) {
         )}
       </span>
       {/* </div> */}
-      <span className="flex flex-col pl-2">
+      <span className="flex flex-col pl-2 text-left">
         <p>{name}</p>
         <p>@{username}</p>
       </span>
