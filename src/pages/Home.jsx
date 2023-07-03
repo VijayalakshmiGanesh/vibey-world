012a761d9cdeaf19585ab297238f3005dd96ebdf
Vijayalakshmi G (she/her) <104077094+VijayalakshmiGanesh@users.noreply.github.com>
2023-07-03 12:34:28 +0530

    Added: cloudinary, emoji, loader, css changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,17 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react";
 import { MdOutlineTune } from "react-icons/md";
-import { AiFillCaretDown, AiFillCaretUp } from "react-icons/ai";
+import {
+  AiFillCaretDown,
+  AiFillCaretUp,
+  AiFillCloseCircle,
+} from "react-icons/ai";
 import { BiTrendingUp } from "react-icons/bi";
+import { BsImage, BsEmojiSmile } from "react-icons/bs";
+// import { MdOutlineEmojiEmotions } from "react-icons/md";
 import { v4 as uuid } from "uuid";
 import Avatar from "react-avatar";
-import { AdvancedImage } from "@cloudinary/react";
-import { Cloudinary } from "@cloudinary/url-gen";
-import Axios from "axios";
+import EmojiPicker from "emoji-picker-react";
 
 import { getAllPosts, newPost } from "../services/Posts";
 import { useData } from "../context/DataContext";
@@ -17,6 +21,7 @@ import { getAllUsers } from "../services/Auth";
 import PostCard from "../component/PostCard";
 import NavBar from "../component/NavBar";
 import Aside from "../component/Aside";
+import Loader from "../component/Loaders";
 
 function Home() {
   const { posts, datadispatch } = useData();
@@ -25,32 +30,30 @@ function Home() {
   const [displaySortOption, setDisplaySortOption] = useState(false);
   const [filteredPosts, setFilteredPosts] = useState(posts);
   const [newPostContent, setNewPostContent] = useState("");
-  const [filesSelected, setFilesSelected] = useState();
-
-  // Create a Cloudinary instance and set your cloud name.
-  const cld = new Cloudinary({ cloud: { cloudName: "dc1hrpwac" } });
-
-  // cld.image returns a CloudinaryImage with the configuration set.
-  const myImage = cld.image("sample");
-
-  // The URL of the image is: https://res.cloudinary.com/demo/image/upload/sample
+  const [filesSelected, setFilesSelected] = useState("");
+  const [isEmojiKeypadDisplayed, setIsEmomjiKeypadDisplayed] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const filterPosts = () => {
     const currentFollowing = currentUserDetails?.following?.map(
       ({ username }) => username
     );
-    if (currentFollowing.length === 0) return [];
+    // if (currentFollowing.length === 0)
+    //   return posts.filter(
+    //     ({ username }) => currentUserDetails.username === username
+    //   );
 
-    console.log("Cuureent followings", currentFollowing);
-    console.log(
-      "filtering",
-      posts.filter(({ username }) => currentFollowing.includes(username))
-    );
     const filteredPostsBasedOnFollowing =
       currentFollowing?.length >= 1
-        ? posts?.filter(({ username }) => username?.includes(currentFollowing))
-        : [];
-    console.log("filteredPostsBasedOnFollowing", filteredPostsBasedOnFollowing);
+        ? posts.filter(
+            ({ username }) =>
+              currentFollowing.includes(username) ||
+              currentUserDetails.username === username
+          )
+        : posts.filter(
+            ({ username }) => currentUserDetails.username === username
+          );
+
     return [
       ...filteredPostsBasedOnFollowing?.sort((a, b) => {
         if (sortPostsBy === "Oldest") {
@@ -66,25 +69,7 @@ function Home() {
     ];
   };
 
-  // var myWidget = cloudinary.createUploadWidget({
-  //   cloudName: 'my_cloud_name',
-  //   uploadPreset: 'my_preset'}, (error, result) => {
-  //     if (!error && result && result.event === "success") {
-  //       console.log('Done! Here is the image info: ', result.info);
-  //     }
-  //   }
-  // )
-
   const uploadImg = async () => {
-    // const formData = new FormData();
-    // formData.append("file", filesSelected[0]);
-    // formData.append("upload_preset", "kdusi1ol");
-
-    // const response = await Axios.post(
-    //   "https://api.cloudinary.com/v1_1/dc1hrpwac/image/upload",
-    //   formData
-    // );
-
     const formData = new FormData();
 
     const CLOUDINARY_URL =
@@ -107,135 +92,220 @@ function Home() {
   };
 
   const newPostHander = async () => {
-    let response = "";
+    if (newPostContent.length >= 1 || filesSelected.length >= 1) {
+      let response = "";
 
-    if (filesSelected[0] !== "") {
-      response = await uploadImg();
-      newPost(
-        {
-          _id: uuid(),
-          content: newPostContent,
-          likes: {
-            likeCount: 0,
-            likedBy: [],
-            dislikedBy: [],
+      if (filesSelected[0] !== "") {
+        response = await uploadImg();
+        newPost(
+          {
+            _id: uuid(),
+            content: newPostContent,
+            likes: {
+              likeCount: 0,
+              likedBy: [],
+              dislikedBy: [],
+            },
+            username: currentUserDetails.username,
+            createdAt: formatDate(),
+            updatedAt: formatDate(),
+            imgURL: response?.url,
           },
-          username: currentUserDetails.username,
-          createdAt: formatDate(),
-          updatedAt: formatDate(),
-          imgURL: response?.url,
-        },
-        datadispatch
-      );
-    } else {
-      newPost(
-        {
-          _id: uuid(),
-          content: newPostContent,
-          likes: {
-            likeCount: 0,
-            likedBy: [],
-            dislikedBy: [],
+          datadispatch
+        );
+      } else {
+        newPost(
+          {
+            _id: uuid(),
+            content: newPostContent,
+            likes: {
+              likeCount: 0,
+              likedBy: [],
+              dislikedBy: [],
+            },
+            username: currentUserDetails.username,
+            createdAt: formatDate(),
+            updatedAt: formatDate(),
+            imgURL: "",
           },
-          username: currentUserDetails.username,
-          createdAt: formatDate(),
-          updatedAt: formatDate(),
-          imgURL: "",
-        },
-        datadispatch
-      );
+          datadispatch
+        );
+      }
+      setFilesSelected("");
+      setNewPostContent("");
+      setIsEmomjiKeypadDisplayed(false);
     }
-    setFilesSelected("");
-    setNewPostContent("");
   };
-
-  console.log("posts", posts);
 
   useEffect(() => {
     setFilteredPosts([...filterPosts()]);
   }, [sortPostsBy, posts, currentUserDetails?.following]);
 
   useEffect(() => {
+    setIsLoading(true);
     getAllPosts(datadispatch);
     getAllUsers(userDispatch);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1500);
   }, []);
   return (
-    <div className="flex justify-center">
-      <NavBar />
-      <div>
-        <div>
-          <div className="flex justify-start border border-2 p-3 my-3 mx-5">
-            <Avatar
-              name={`${currentUserDetails.firstName} ${currentUserDetails.lastName}`}
-              round={true}
-              size="50"
-              color={Avatar.getRandomColor("sitebase", [
-                // "rgb(251 146 60)",
-                "black",
-
-                "blue",
-                "pink",
-              ])}
-            />
-            <div className="relative grow">
-              <textarea
-                className=" my-2 py-2 input-text border-0 resize-none w-full bg-inherit focus-visible:border-0 focus:outline-0"
-                placeholder="What's Happening?!"
-                value={newPostContent}
-                onChange={(e) => setNewPostContent(e.target.value)}
-              ></textarea>
-              <input
-                type="file"
-                // value={file}
-                onChange={(e) => setFilesSelected(e.target.files)}
-              />
-              <button
-                className="absolute right-0 bottom-[-15px] btn-primary w-auto"
-                onClick={() => newPostHander()}
-              >
-                POST
-              </button>
-            </div>
-          </div>
-          <span>{sortPostsBy}</span>
-          <button onClick={() => setDisplaySortOption((prev) => !prev)}>
-            <MdOutlineTune />
-          </button>
-          {displaySortOption && (
+    <div>
+      {/* // <div className="flex justify-start">
+    //   <div className=" px-5 pr-[13rem]">
+    //     <NavBar />
+    //   </div> */}
+      <div className="w-[40rem] ">
+        {isLoading ? (
+          <span className="flex items-center justify-center h-screen text-2xl">
+            {" "}
+            <Loader />
+          </span>
+        ) : (
+          <>
             <div>
-              <button onClick={() => setSortPostsBy("Trending")}>
-                Trending
-                <span>
-                  <BiTrendingUp />
-                </span>
-              </button>
-              <button onClick={() => setSortPostsBy("Latest")}>
-                Newest
-                <span>
-                  <AiFillCaretUp />
-                </span>
-              </button>
-              <button onClick={() => setSortPostsBy("Oldest")}>
-                Oldest
-                <span>
-                  <AiFillCaretDown />
-                </span>
-              </button>
+              <div className="flex justify-start border border-slate-400 p-3 my-3 mx-5">
+                <Avatar
+                  name={`${currentUserDetails.firstName} ${currentUserDetails.lastName}`}
+                  round={true}
+                  size="50"
+                  color={Avatar.getRandomColor("sitebase", [
+                    // "rgb(251 146 60)",
+                    "black",
+
+                    "blue",
+                    "pink",
+                  ])}
+                />
+                <div className="relative grow w-[35rem]">
+                  <textarea
+                    className=" my-2 py-2 input-text border-0 resize-none w-full bg-inherit focus-visible:border-0 focus:outline-0"
+                    placeholder="What's Happening?!"
+                    value={newPostContent}
+                    onChange={(e) => setNewPostContent(e.target.value)}
+                  ></textarea>
+                  <div className="flex justify-center ">
+                    {filesSelected !== "" && filesSelected?.length !== 0 && (
+                      <span className="relative">
+                        <img
+                          src={URL.createObjectURL(filesSelected[0])}
+                          alt="file selected"
+                          height="300"
+                          width="300"
+                        />
+
+                        <button
+                          className="absolute top-[-0.5rem] right-[-0.4rem] text-red-800 text-2xl"
+                          onClick={() => setFilesSelected("")}
+                        >
+                          {<AiFillCloseCircle />}
+                        </button>
+                      </span>
+                    )}
+                  </div>
+                  <div className="flex items-center">
+                    <label className="cursor-pointer text-lg">
+                      <BsImage />
+                      <input
+                        type="file"
+                        className="hidden"
+                        onChange={(e) => setFilesSelected(e.target.files)}
+                      />
+                    </label>
+
+                    <span
+                      className={`relative self-end px-4 pt-[2px] text-lg hover:text-yellow-400 ${
+                        isEmojiKeypadDisplayed && "text-yellow-400"
+                      }`}
+                    >
+                      <button
+                        onClick={() =>
+                          setIsEmomjiKeypadDisplayed((prev) => !prev)
+                        }
+                      >
+                        <BsEmojiSmile />
+                      </button>
+                      {isEmojiKeypadDisplayed && (
+                        <span className="absolute top-5  right-2 mx-auto">
+                          <EmojiPicker
+                            onEmojiClick={(e) =>
+                              setNewPostContent((prev) => prev.concat(e.emoji))
+                            }
+                          />
+                        </span>
+                      )}
+                    </span>
+                    {/* </label> */}
+                    <button
+                      className="absolute right-0 bottom-[-15px] btn-primary w-auto"
+                      onClick={() => newPostHander()}
+                    >
+                      POST
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <p className="flex items-center justify-between px-5 py-3">
+                <span className="text-xl">{sortPostsBy} Posts</span>
+                <div>
+                  <button
+                    onClick={() => setDisplaySortOption((prev) => !prev)}
+                    className="text-xl hover:surface-clr hover:rounded-full hover:text-pink-400 p-2"
+                  >
+                    <MdOutlineTune />
+                  </button>
+                  {displaySortOption && (
+                    <div className="relative">
+                      <div className="flex flex-col absolute top-0 right-3 w-[7rem] h-fit flex flex-col border border-2 z-30 bg-primary rounded">
+                        <button
+                          onClick={() => setSortPostsBy("Trending")}
+                          className="flex items-center p-2  border-b-2 hover:surface-clr"
+                        >
+                          <span className="pr-2">
+                            <BiTrendingUp />
+                          </span>
+                          Trending
+                        </button>
+                        <button
+                          onClick={() => setSortPostsBy("Latest")}
+                          className="flex items-center p-2  border-b-2 hover:surface-clr"
+                        >
+                          <span className="pr-2">
+                            <AiFillCaretUp />
+                          </span>
+                          Latest
+                        </button>
+                        <button
+                          onClick={() => setSortPostsBy("Oldest")}
+                          className="flex items-center p-2  border-b-2 hover:surface-clr"
+                        >
+                          <span className="pr-2">
+                            <AiFillCaretDown />
+                          </span>
+                          Oldest
+                        </button>
+                      </div>
+                    </div>
+                  )}
+                </div>
+              </p>
             </div>
-          )}
-        </div>
-        <div className="flex flex-col items-center m-2 flex-wrap">
-          {filteredPosts?.length === 0 ? (
-            <p>No Posts</p>
-          ) : (
-            filteredPosts?.map((post) => {
-              return <PostCard post={post} />;
-            })
-          )}
-          <AdvancedImage cldImg={myImage} />
-        </div>
+            <div className="flex flex-col items-center m-2 flex-wrap">
+              {filteredPosts?.length === 0 ? (
+                <p>No Posts</p>
+              ) : (
+                filteredPosts?.map((post) => {
+                  return <PostCard post={post} />;
+                })
+              )}
+              {/* <AdvancedImage cldImg={myImage} /> */}
+            </div>
+          </>
+        )}
       </div>
-      <Aside />
+      {/* <div>
+        <Aside />
+      </div> */}
     </div>
   );
 }
